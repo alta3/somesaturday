d80496747b97f585c280bba32ad44029c757f697
framer <framer@somesaturday.somesaturday.com>
2015-02-14 19:40:20 -0500

    Added VIPER

--- somesaturday.go
+++ somesaturday.go
@@ -3,14 +3,39 @@ package main
 import (
     "log"
     "net/http"
+    "github.com/spf13/viper"
 )
 
+
+var config = struct {
+	BasePath             string
+	HostName             string
+	SalesEmail           string
+	Verbose              bool
+}{}
+
+
+
+func ParseConfig(configFile string) {
+	viper.SetConfigFile(configFile)
+       viper.AddConfigPath("/deploy")
+	if err := viper.ReadInConfig(); err != nil {
+		log.Fatal("Viper failed: ", err)
+	}
+	config.BasePath = viper.GetString("BasePath")
+	config.HostName = viper.GetString("HostName")
+	config.SalesEmail = viper.GetString("SalesEmail")
+	config.Verbose = viper.GetBool("Verbose")
+}
+
+
 func main() {
+    ParseConfig("deploy/config.yaml") // context.go
     http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("pages/"))))
     http.Handle("/Images/", http.StripPrefix("/Images/", http.FileServer(http.Dir("Images"))))
     http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer(http.Dir("styles"))))
     http.Handle("/Lightbox/", http.StripPrefix("/Lightbox/", http.FileServer(http.Dir("Lightbox"))))
-    if err := http.ListenAndServe("192.168.241.22:80", nil); err != nil {
-        log.Fatal("ListenAndServe: ", err)
+    if err := http.ListenAndServe(config.HostName, nil); err != nil {
+    log.Fatal("ListenAndServe: ", err)
     }
 }
