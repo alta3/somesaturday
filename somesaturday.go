c22c3014c17045f2c93890774e21a54c0c52415f
framer <framer@somesaturday.somesaturday.com>
2015-02-15 22:21:07 -0500

    Added VIPER search path and made config a type. This should close #3

--- somesaturday.go
+++ somesaturday.go
@@ -1,24 +1,26 @@
 package main
 
 import (
-    "log"
-    "net/http"
-    "github.com/spf13/viper"
+	"github.com/spf13/viper"
+	"log"
+	"net/http"
 )
 
+type Config struct {
+	BasePath   string
+	HostName   string
+	SalesEmail string
+	Verbose    bool
+	Hours      string
+}
 
-var config = struct {
-	BasePath             string
-	HostName             string
-	SalesEmail           string
-	Verbose              bool
-}{}
+var config Config
 
-
-//Learning to add viper and close a commit at the same time
 func ParseConfig(configFile string) {
-	viper.SetConfigFile(configFile)
-       viper.AddConfigPath("/deploy")
+	var config Config
+	viper.SetConfigName(configFile)
+	viper.AddConfigPath("/opt/deploy/")
+	viper.AddConfigPath("$HOME/go/src/github.com/golang/somesaturday.com/deploy/")
 	if err := viper.ReadInConfig(); err != nil {
 		log.Fatal("Viper failed: ", err)
 	}
@@ -28,14 +30,13 @@ func ParseConfig(configFile string) {
 	config.Verbose = viper.GetBool("Verbose")
 }
 
-
 func main() {
-    ParseConfig("deploy/config.yaml") // context.go
-    http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("pages/"))))
-    http.Handle("/Images/", http.StripPrefix("/Images/", http.FileServer(http.Dir("Images"))))
-    http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer(http.Dir("styles"))))
-    http.Handle("/Lightbox/", http.StripPrefix("/Lightbox/", http.FileServer(http.Dir("Lightbox"))))
-    if err := http.ListenAndServe(config.HostName, nil); err != nil {
-    log.Fatal("ListenAndServe: ", err)
-    }
+	ParseConfig("config")
+	http.Handle("/", http.StripPrefix("/", http.FileServer(http.Dir("pages/"))))
+	http.Handle("/Images/", http.StripPrefix("/Images/", http.FileServer(http.Dir("Images"))))
+	http.Handle("/styles/", http.StripPrefix("/styles/", http.FileServer(http.Dir("styles"))))
+	http.Handle("/Lightbox/", http.StripPrefix("/Lightbox/", http.FileServer(http.Dir("Lightbox"))))
+	if err := http.ListenAndServe(config.HostName, nil); err != nil {
+		log.Fatal("ListenAndServe: ", err)
+	}
 }
